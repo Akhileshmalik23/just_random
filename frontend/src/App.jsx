3de7a7c27718cff6a449cf381bd42f202338d182
abhyuday1212 <apsworks1212@gmail.com>
2024-08-28 00:10:08 +0530

    basic setup added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-red-500">
+      Hello Ji, aapka app chal gya....bht bht bdhaiyan ji
+    </div>
+  );
+};
+
+export default App;
